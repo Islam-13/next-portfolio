a617fabb9d97c86f9d483b8c602f99fa3aae7c26
sarah <islam-hussein@outlook.com>
2025-02-27 14:48:06 +0300

    add angular logo

--- app/_components/Tech.jsx
+++ app/_components/Tech.jsx
@@ -5,15 +5,16 @@ const logos = [
   { src: "/images/tech/nestjs-logo.svg", alt: "Nest logo" },
   { src: "/images/tech/mongodb-logo.svg", alt: "MongoDB logo" },
   { src: "/images/tech/nodejs-logo.svg", alt: "Node logo" },
+  { src: "/images/tech/angular-logo.svg", alt: "Angular logo" },
   { src: "/images/tech/nextjs-logo.svg", alt: "Next logo" },
   { src: "/images/tech/reactjs-logo.svg", alt: "React logo" },
   { src: "/images/tech/jQuery-logo.svg", alt: "jQuery logo" },
+  { src: "/images/tech/typeScript-logo.svg", alt: "Typescript logo" },
   { src: "/images/tech/javaScript-logo.svg", alt: "javaScript logo" },
   { src: "/images/tech/bootstrap-logo.svg", alt: "Bootstrap logo" },
   { src: "/images/tech/tailwind-logo.svg", alt: "Tailwind logo" },
   { src: "/images/tech/css-logo.svg", alt: "Css logo" },
   { src: "/images/tech/html-logo.svg", alt: "Html logo" },
-  { src: "/images/tech/typeScript-logo.svg", alt: "Typescript logo" },
 ];
 
 const settings = {
